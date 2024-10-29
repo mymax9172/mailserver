e0e20ecd14d6bb0dfeac49d849c9e0b61ddbb2a8
Massimiliano Agostinoni <massimiliano@agostinoni.it>
2024-10-29 11:42:18 +0100

    cors

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
 import { router } from "./router.js";
 import { templates } from "./templates.js";
 
 const app = express();
 const port = 3000;
 
+var corsOptions = {
+	origin: /mistercompliance\.it$/,
+};
+
 // Middlewares
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Routing
@@ -16,5 +22,5 @@ templates.load();
 
 // Launch the server
 app.listen(port, () => {
-    console.log("Server is up and running on port %s", port);    
-})
+	console.log("Server is up and running on port %s", port);
+});
