b699fbaf8fcf85e35b24773e95d85909a35fccbb
Massimiliano Agostinoni <massimiliano@agostinoni.it>
2024-10-29 12:00:42 +0100

    bug fixing

--- templates.js
+++ templates.js
@@ -2,41 +2,43 @@ import pug from "pug";
 import fs from "fs";
 
 export const templates = {
-    templates: {},
-    compiledTemplates: {},
+	templates: {},
+	compiledTemplates: {},
 
-    storeTemplate(name, body) {
-        this.templates[name] = body;
-        this.compiledTemplates[name] = pug.compile(body);
-    },
+	storeTemplate(name, body) {
+		this.templates[name] = body;
+		this.compiledTemplates[name] = pug.compile(body);
+	},
 
-    render(name, values) {
-        if (!this.compiledTemplates.hasOwnProperty(name)) throw new Error("Template " + name + " does not exist");
-        const compiledFunction = this.compiledTemplates[name];
-        return compiledFunction(values);
-    },
+	render(name, values) {
+		if (!this.compiledTemplates.hasOwnProperty(name)) {
+			if (!this.templates.hasOwnProperty(name))
+				throw new Error("Template " + name + " does not exist");
+			else this.compiledTemplates[name] = pug.compile(this.templates[name]);
+		}
 
-    save() {
-        fs.writeFileSync("./templates.json", JSON.stringify(this.templates));
-    },
+		const compiledFunction = this.compiledTemplates[name];
+		return compiledFunction(values);
+	},
 
-    load() {
-        if (fs.existsSync("./templates.json")) {
-            console.log("loading file...");
-            const content = fs.readFileSync("./templates.json");
-            this.templates = JSON.parse(content);
-            
-            // TODO parsing...
-            const names = Object.keys(this.templates);
-            names.forEach(name => {
-                console.log('compiling ...' + name)
-                this.compiledTemplates[name] = pug.compile(this.templates[name]);               
-            })
-        } else {
-            this.templates = {}
-        }
-    }
+	save() {
+		fs.writeFileSync("./templates.json", JSON.stringify(this.templates));
+	},
 
+	load() {
+		if (fs.existsSync("./templates.json")) {
+			console.log("loading file...");
+			const content = fs.readFileSync("./templates.json");
+			this.templates = JSON.parse(content);
 
-
-}
+			// TODO parsing...
+			const names = Object.keys(this.templates);
+			names.forEach((name) => {
+				console.log("compiling ..." + name);
+				this.compiledTemplates[name] = pug.compile(this.templates[name]);
+			});
+		} else {
+			this.templates = {};
+		}
+	},
+};
