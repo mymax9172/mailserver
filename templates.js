1527be29104e136b7a0bd2ff64b1e693d6d03eb5
Massimiliano Agostinoni <massimiliano@agostinoni.it>
2024-10-29 13:34:25 +0100

    static templates

--- templates.js
+++ templates.js
@@ -10,6 +10,21 @@ export const templates = {
 		this.compiledTemplates[name] = pug.compile(body);
 	},
 
+	// renderStatic(name, values) {
+	// 	if (!this.compiledTemplates.hasOwnProperty(name)) {
+	// 		const filename = "./templates/" + name + ".pug";
+	// 		if (!fs.existsSync(filename))
+	// 			throw new Error("Static template " + name + " does not exist");
+	// 		else {
+	// 			const content = fs.readFileSync(filename);
+	// 			this.compiledTemplates[name] = pug.compile(content);
+	// 		}
+	// 	}
+
+	// 	const compiledFunction = this.compiledTemplates[name];
+	// 	return compiledFunction(values);
+	// },
+
 	render(name, values) {
 		if (!this.compiledTemplates.hasOwnProperty(name)) {
 			if (!this.templates.hasOwnProperty(name))
@@ -26,8 +41,20 @@ export const templates = {
 	},
 
 	load() {
+		// Read static templates (if any)
+		const fileList = fs.readdirSync("./templates");
+		console.log("loading /templates folder...", fileList);
+		for (const file of fileList) {
+			const filename = `./templates/${file}`;
+			const content = fs.readFileSync(filename);
+			const name = file.split(".")[0];
+			console.log("compiling ..." + name);
+			this.compiledTemplates[name] = pug.compile(content);
+		}
+
+		// Read dynamic templates (if any)
 		if (fs.existsSync("./templates.json")) {
-			console.log("loading file...");
+			console.log("loading templates.json...");
 			const content = fs.readFileSync("./templates.json");
 			this.templates = JSON.parse(content);
 
