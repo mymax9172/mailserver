ba5c27f089a71aeaa58ace922594c2a28d5af802
Massimiliano Agostinoni <massimiliano@agostinoni.it>
2024-10-30 08:09:05 +0100

    templates

--- templates.js
+++ templates.js
@@ -10,21 +10,6 @@ export const templates = {
 		this.compiledTemplates[name] = pug.compile(body);
 	},
 
-	// renderStatic(name, values) {
-	// 	if (!this.compiledTemplates.hasOwnProperty(name)) {
-	// 		const filename = "./templates/" + name + ".pug";
-	// 		if (!fs.existsSync(filename))
-	// 			throw new Error("Static template " + name + " does not exist");
-	// 		else {
-	// 			const content = fs.readFileSync(filename);
-	// 			this.compiledTemplates[name] = pug.compile(content);
-	// 		}
-	// 	}
-
-	// 	const compiledFunction = this.compiledTemplates[name];
-	// 	return compiledFunction(values);
-	// },
-
 	render(name, values) {
 		if (!this.compiledTemplates.hasOwnProperty(name)) {
 			if (!this.templates.hasOwnProperty(name))
@@ -45,13 +30,14 @@ export const templates = {
 		const fileList = fs.readdirSync("./templates");
 		console.log("loading /templates folder...", fileList);
 		for (const file of fileList) {
-			const filename = `./templates/${file}`;
-			const content = fs.readFileSync(filename);
-			const name = file.split(".")[0];
-			console.log("compiling ..." + name);
-			this.compiledTemplates[name] = pug.compile(content);
+			if (file.endsWith(".pug")) {
+				const filename = `./templates/${file}`;
+				const content = fs.readFileSync(filename);
+				const name = file.split(".")[0];
+				console.log("compiling ..." + name);
+				this.compiledTemplates[name] = pug.compile(content);
+			}
 		}
-
 		// Read dynamic templates (if any)
 		if (fs.existsSync("./templates.json")) {
 			console.log("loading templates.json...");
